5b8bc72c894b5479ec20642a2457631885b2b57e
shanekeran <shanekeran@gmail.com>
2021-04-05 16:10:36 +0000

    Add Nav bar to base.html

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+  // Collapsed Mobile Navigation bar (Materialize)
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, { edge: "right"});
+  });
