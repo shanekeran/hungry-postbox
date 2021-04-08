38335a63ce34ade35a6343270ac7a67e19fbf7e9
shanekeran <shanekeran@gmail.com>
2021-04-08 14:55:35 +0000

    Add carousel for user profiles in discover section.

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,8 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, { edge: "right"});
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {});
+  });
