bb03d03b9c3127b61cbd19dd5484aee819863ce6
shanekeran <shanekeran@gmail.com>
2021-04-10 14:36:30 +0000

    Add progress bar to registration form

--- static/js/script.js
+++ static/js/script.js
@@ -59,23 +59,24 @@
 
   function validateForm() {
     // This function deals with validation of the form fields
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("form-page");
-    y = x[currentPage].getElementsByTagName("input");
+    var form, formInputs, i, valid = true;
+    form = document.getElementsByClassName("form-page");
+    formInputs = form[currentPage].getElementsByTagName("input");
     // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
+    for (i = 0; i < formInputs.length; i++) {
       // If a field is empty...
-      if (y[i].value == "") {
+      if (formInputs[i].value == "") {
         // add an "invalid" class to the field:
-        y[i].className += " invalid";
+        formInputs[i].className += " invalid";
         // and set the current valid status to false:
         valid = false;
       }
     }
     // If the valid status is true, mark the step as finished and valid:
     if (valid) {
-      document.getElementsByClassName("step")[currentPage].className += " finish";
+        document.getElementsByClassName("step")[currentPage].className = " step finish fas fa-check-circle";
     }
+    
     return valid; // return the valid status
   }
 
@@ -83,8 +84,8 @@
     // This function removes the "active" class of all steps...
     var i, x = document.getElementsByClassName("step");
     for (i = 0; i < x.length; i++) {
-      x[i].className = x[i].className.replace(" active", "");
+      x[i].className = x[i].className.replace(" active fas fa-dot-circle", "");
     }
     //... and adds the "active" class to the current step:
-    x[n].className += " active";
+    x[n].className += " active fas fa-dot-circle";
   }
