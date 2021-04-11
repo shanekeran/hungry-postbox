e8e5312fcddc1ab2cde59a160b004a42197b2dcf
shanekeran <shanekeran@gmail.com>
2021-04-11 09:23:15 +0000

    Fix issue that allows users to progress on form with invalid input fields.

--- static/js/script.js
+++ static/js/script.js
@@ -16,39 +16,40 @@
       var instances = M.Datepicker.init(elems, {format: 'dd/mm/yyyy'});
     });
   
-    var currentPage = 0; // Current tab is set to be the first tab (0)
-    showPage(currentPage); // Display the current tab
+  // Multi step form, original code sourced from w3schools and modified (see README).
+  var currentPage = 0;
+  showPage(currentPage); // Display the current page
 
   function showPage(n) {
-    // This function will display the specified tab of the form ...
-    var page = document.getElementsByClassName("form-page");
-    page[n].style.display = "block";
-    // ... and fix the Previous/Next buttons:
+    // This function will display the specified form page while the others are hidden
+    var pages = document.getElementsByClassName("form-page");
+    pages[n].style.display = "block";
+    // Prev/Next buttons
     if (n == 0) {
-      document.getElementById("prevBtn").style.display = "none";
+      document.getElementById("prevBtn").style.display = "none"; // On the first page, prev btn is hidden.
     } else {
       document.getElementById("prevBtn").style.display = "inline";
     }
-    if (n == (page.length - 1)) {
-      document.getElementById("nextBtn").innerHTML = "Submit";
+    if (n == (pages.length - 1)) {
+      document.getElementById("nextBtn").innerHTML = "Register"; // on the last page, next is replaced with Register.
     } else {
       document.getElementById("nextBtn").innerHTML = "Next";
     }
-    // ... and run a function that displays the correct step indicator:
-    fixStepIndicator(n)
+    // ... and run a function that displays the correct page indicator:
+    updateIndicators(n)
   }
 
   function nextPrev(n) {
     // This function will figure out which tab to display
-    var x = document.getElementsByClassName("form-page");
+    var pages = document.getElementsByClassName("form-page");
     // Exit the function if any field in the current tab is invalid:
     if (n == 1 && !validateForm()) return false;
     // Hide the current tab:
-    x[currentPage].style.display = "none";
+    pages[currentPage].style.display = "none";
     // Increase or decrease the current tab by 1:
     currentPage = currentPage + n;
     // if you have reached the end of the form... :
-    if (currentPage >= x.length) {
+    if (currentPage >= pages.length) {
       //...the form gets submitted:
       document.getElementById("regForm").submit();
       return false;
@@ -59,33 +60,30 @@
 
   function validateForm() {
     // This function deals with validation of the form fields
-    var form, formInputs, i, valid = true;
+    var form, formInputs, valid = true;
     form = document.getElementsByClassName("form-page");
     formInputs = form[currentPage].getElementsByTagName("input");
     // A loop that checks every input field in the current tab:
     for (i = 0; i < formInputs.length; i++) {
-      // If a field is empty...
-      if (formInputs[i].value == "") {
-        // add an "invalid" class to the field:
-        formInputs[i].className += " invalid";
-        // and set the current valid status to false:
+      // If a field is empty: valid is set to false to prevent user from advancing.
+      if (formInputs[i].value == "" || formInputs[i].validity.tooShort) {
         valid = false;
       }
     }
-    // If the valid status is true, mark the step as finished and valid:
+    // If the valid status is true, the user can advance and a visual tick icon is displayed on progress bar.
     if (valid) {
-        document.getElementsByClassName("step")[currentPage].className = " step finish fas fa-check-circle";
+        document.getElementsByClassName("indicator")[currentPage].className = " indicator finish fas fa-check-circle";
     }
     
     return valid; // return the valid status
   }
 
-  function fixStepIndicator(n) {
-    // This function removes the "active" class of all steps...
-    var i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) {
-      x[i].className = x[i].className.replace(" active fas fa-dot-circle", "");
+  function updateIndicators(n) {
+    // Removes the "active" class off all steps...
+    var indicator = document.getElementsByClassName("indicator");
+    for (i = 0; i < indicator.length; i++) {
+      indicator[i].className = indicator[i].className.replace(" active fas fa-dot-circle", "");
     }
     //... and adds the "active" class to the current step:
-    x[n].className += " active fas fa-dot-circle";
+    indicator[n].className += " active fas fa-dot-circle";
   }
