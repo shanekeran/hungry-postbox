0f74587a851f226803683591a2a028b559d15a61
shanekeran <shanekeran@gmail.com>
2021-04-26 08:16:08 +0000

    Add member page to app.py and nav bar. Fix styling on nav bar to accomodate extra nav link

--- app.py
+++ app.py
@@ -182,6 +182,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/members")
+def members():
+    return render_template("members.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
